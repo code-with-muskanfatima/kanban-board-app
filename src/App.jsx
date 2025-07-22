360f1cf1eb7c24c77af5537f3cae991e9d2600f8
muskanfat <mfw6950622@gmail.com>
2025-07-21 21:43:28 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 
 // ✅ Import your components
 import Login from "./pages/Login";
